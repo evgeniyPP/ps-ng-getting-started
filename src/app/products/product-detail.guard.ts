0014e2e8acd60a07f2543fc2333a6ca4fe51e6c9
evgeniyPP <aysanru@gmail.com>
2020-04-14 20:09:09 +0300

    Routing & Product Detail Page

---
+++ src/app/products/product-detail.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, ActivatedRouteSnapshot, Router } from "@angular/router";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ProductDetailGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(next: ActivatedRouteSnapshot): boolean {
+    let id = +next.url[1].path;
+    if (isNaN(id) || id < 1) {
+      alert("Invalid product id");
+      this.router.navigate(["/products"]);
+      return false;
+    }
+    return true;
+  }
+}
